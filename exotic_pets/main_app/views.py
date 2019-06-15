b67cee304035db79b9ce1cc8f5e6c4c23feddf1f
Tatty <tattyklass@gmail.com>
2019-06-15 15:58:57 -0700

    fixed update/delete functionality

--- exotic_pets/main_app/views.py
+++ exotic_pets/main_app/views.py
@@ -19,10 +19,10 @@ def home(request):
     return render(request, 'home.html')
 
 def posts_index(request):
-    posts=Post.objects.all()
+    posts = Post.objects.all()
     return render(request, 'posts/index.html', {'posts': posts})
 
 def posts_detail(request, post_id):
-    post = []
+    post = Post.objects.get(id=post_id)
     return render(request, 'posts/detail.html', {'post': post})
 
