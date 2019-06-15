27e517c979ff3dad3358c53559a3a991d003c6cc
mykindofscum <alvinkchan99@gmail.com>
2019-06-14 18:24:26 -0700

    Add postdetail route

--- exotic_pets/main_app/urls.py
+++ exotic_pets/main_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('posts/', views.posts_index, name='index'),
+    path('posts/<int:post_id>/', views.posts_detail, name='detail'),
 ]
