80d5f790f8610fe02089b3559cd0a7bacfd61eb3
Tatty <tattyklass@gmail.com>
2019-06-16 12:45:59 -0700

    add authorization

--- exotic_pets/main_app/views.py
+++ exotic_pets/main_app/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post, Photo
 import uuid
 import boto3
+
 S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
 BUCKET = 'catcollector5'
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'description', 'price', 'contact']
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
     fields = '__all__'
-    success_url ='/posts/'
 
-class PostUpdate(UpdateView):
-    model = Post
-    fields = '__all__'
-
-class PostDelete(DeleteView):
+class PostDelete(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = '/posts/'
 
@@ -26,10 +33,17 @@ def posts_index(request):
     posts = Post.objects.all()
     return render(request, 'posts/index.html', {'posts': posts})
 
+@login_required
 def posts_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, 'posts/detail.html', {'post': post})
 
+@login_required
+def user_index(request):
+    posts = Post.objects.filter(user=request.user)
+    return render(request, 'main_app/user_index.html', {'posts': posts})
+
+@login_required
 def add_photo(request, post_id):
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
@@ -43,3 +57,17 @@ def add_photo(request, post_id):
         except:
             print('An error occurred uploading file to S3')
     return redirect('detail', post_id=post_id)
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
