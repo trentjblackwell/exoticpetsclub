1bbffff85cc9935a4853f76945d356e546d1d213
TrentB <trentjblackwell@gmail.com>
2019-06-20 11:42:29 -0700

    bug fix

--- main_app/static/main.js
+++ main_app/static/main.js
@@ -8,3 +8,11 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.dropdown-trigger');
     var instances = M.Dropdown.init(elems);
   });
+
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+  });
+
+  var instance = M.Sidenav.getInstance(elem);
